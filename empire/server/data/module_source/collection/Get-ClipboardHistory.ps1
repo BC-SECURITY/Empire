46a1000bc1f4075e8415223d8d0882147bc7d6b4
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2026-01-20 19:42:13 +0100

    feat: Update error messages
    Either the clipboard history is disabled, or the clipboard is empty
    Some code refactor has also be done

--- empire/server/data/module_source/collection/Get-ClipboardHistory.ps1
+++ empire/server/data/module_source/collection/Get-ClipboardHistory.ps1
@@ -1,22 +1,48 @@
-function Get-ClipboardHistory() {
+function Get-ClipboardHistory {
+
+    $ErrorActionPreference = 'Stop'
+
+    function Fail($msg) { throw $msg }
 
     Add-Type -AssemblyName System.Runtime.WindowsRuntime
 
-    $asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+    $asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() |
+        Where-Object {
+            $_.Name -eq 'AsTask' -and
+            $_.GetParameters().Count -eq 1 -and
+            $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1'
+        })[0]
 
     function Await($WinRtTask, $ResultType) {
-        $asTask = $asTaskGeneric.MakeGenericMethod($ResultType)
+        $asTask  = $asTaskGeneric.MakeGenericMethod($ResultType)
         $netTask = $asTask.Invoke($null, @($WinRtTask))
-        $netTask.Wait(-1) | Out-Null
+        $null = $netTask.Wait(-1)
         $netTask.Result
     }
 
     $null = [Windows.ApplicationModel.DataTransfer.Clipboard, Windows.ApplicationModel.DataTransfer, ContentType=WindowsRuntime]
-    $op = [Windows.ApplicationModel.DataTransfer.Clipboard]::GetHistoryItemsAsync()
 
-    $result = Await ($op) `
-        ([Windows.ApplicationModel.DataTransfer.ClipboardHistoryItemsResult])
+    $result = Await (
+        [Windows.ApplicationModel.DataTransfer.Clipboard]::GetHistoryItemsAsync()
+    ) ([Windows.ApplicationModel.DataTransfer.ClipboardHistoryItemsResult])
 
-    $textops = $result.Items.Content.GetTextAsync()
-    for ($i = 0; $i -lt $textops.Count; $i++){ Await($textops[$i]) ([String]) }
+    if ($result.Status -ne [Windows.ApplicationModel.DataTransfer.ClipboardHistoryItemsResultStatus]::Success) {
+        Fail "ClipboardHistory is not accessible, it might not be enabled. Status: $($result.Status)"
+    }
+
+    try {
+
+        $textOps = $result.Items.Content.GetTextAsync()
+
+        $out = New-Object System.Collections.Generic.List[string]
+        for ($i = 0; $i -lt $textOps.Count; $i++) {
+            $txt = Await ($textOps[$i]) ([string])
+            if ($txt) { $out.Add("---`n$txt") }
+        }
+
+        Write-Output $out
+
+    } catch {
+        Fail "Clipboard is empty."
+    }
 }
