2f2a3cdde47f86c1ac1523a7c2700acb462d5a2a
Andrei SÂNTOMA <andreisantoma@gmail.com>
2026-02-27 16:07:02 +0200

    Make boolean parameter strings received from starkiller lowercase

--- empire/server/modules/bof/credentials/nanodump.py
+++ empire/server/modules/bof/credentials/nanodump.py
@@ -17,32 +17,36 @@ class Module:
             "DumpPath": params.get("write", "find_me.dmp"),
             "WriteFile": "1" if params.get("write") else "0",
             "Chunksize": params.get("chunksize") if params.get("chunksize") else "0",
-            "ValidSignature": "1" if params.get("valid") == "true" else "0",
-            "Fork": "1" if params.get("fork") == "true" else "0",
-            "Snapshot": "1" if params.get("snapshot") == "true" else "0",
-            "DuplicateHandle": "1" if params.get("duplicate") == "true" else "0",
-            "ElevateHandle": "1" if params.get("elevate-handle") == "true" else "0",
+            "ValidSignature": "1" if params.get("valid").lower() == "true" else "0",
+            "Fork": "1" if params.get("fork").lower() == "true" else "0",
+            "Snapshot": "1" if params.get("snapshot").lower() == "true" else "0",
+            "DuplicateHandle": "1" if params.get("duplicate").lower() == "true" else "0",
+            "ElevateHandle": "1" if params.get("elevate-handle").lower() == "true" else "0",
             "DuplicateElevate": (
-                "1" if params.get("duplicate-elevate") == "true" else "0"
+                "1" if params.get("duplicate-elevate").lower() == "true" else "0"
             ),
-            "GetPID": "1" if params.get("getpid") == "true" else "0",
+            "GetPID": "1" if params.get("getpid").lower() == "true" else "0",
             "SecLogonLeakLocal": (
-                "1" if params.get("seclogon-leak-local") == "true" else "0"
+                "1" if params.get("seclogon-leak-local").lower() == "true" else "0"
             ),
             "SecLogonLeakRemote": (
-                "1" if params.get("seclogon-leak-remote") == "true" else "0"
+                "1" if params.get("seclogon-leak-remote").lower() == "true" else "0"
             ),
             "SecLogonLeakRemoteBinary": (
-                "0" if params.get("seclogon-leak-remote") == "true" else ""
+                "0" if params.get("seclogon-leak-remote").lower() == "true" else ""
             ),
             "SecLogonDuplicate": (
-                "1" if params.get("seclogon-duplicate") == "true" else "0"
+                "1" if params.get("seclogon-duplicate").lower() == "true" else "0"
             ),
-            "SpoofCallstack": "1" if params.get("spoof-callstack") == "true" else "0",
-            "SilentProcessExit": "1" if params.get("silent-process-exit") else "0",
+            "SpoofCallstack": "1" if params.get("spoof-callstack").lower() == "true" else "0",
+            "SilentProcessExit": "1" if params.get("silent-process-exit").lower() else "0",
             "SilentProcessExitBinary": params.get("silent-process-exit", ""),
-            "Shtinkering": "1" if params.get("shtinkering") == "true" else "0",
+            "Shtinkering": "1" if params.get("shtinkering").lower() == "true" else "0",
         }
+
+        import debugpy
+        # debugpy.wait_for_client()
+        # breakpoint()
 
         return main_menu.modulesv2.generate_script_bof(
             module=module,
